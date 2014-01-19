ebfa24bec590a01adb7ef14111c7ed2415bbe0bd
David W. Body <davidbody@bigcreek.com>
2014-01-19 15:04:14 -0600

    Added watchers and cleaned up code

--- public/js/app.js
+++ public/js/app.js
@@ -3,26 +3,40 @@
 var mathApp = angular.module('mathApp', []);
 
 mathApp.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.params = {
+  };
 
-    $scope.calculate = function() {
-      var params;
+  $scope.$watch('params.n1', function(newValue, oldValue) {
+    if (!$scope.n2Overridden) {
+      $scope.params.n2 = newValue;
+    }
+  });
 
-      $scope.result = '';
-      $scope.errors = '';
+  $scope.$watch('params.n2', function(newValue, oldValue) {
+    if ($scope.params.n2 != $scope.params.n1) {
+      $scope.n2Overridden = true;
+    }
+  });
 
-      $http.get('/calculate', {params: $scope.params}).
-        success(function(data, status, headers, config) {
-          $scope.result = data.result;
-        }).
-        error(function(data, status, headers, config) {
-          if (data.errors) {
-            $scope.errors = data.errors;
-          } else {
-            alert("Something went wrong.");
-          }
-        });
-    };
+  $scope.calculate = function() {
+    var params;
+
+    $scope.result = '';
+    $scope.errors = '';
+
+    $http.get('/calculate', {params: $scope.params}).
+      success(function(data, status, headers, config) {
+        $scope.result = data.result;
+      }).
+      error(function(data, status, headers, config) {
+        if (data.errors) {
+          $scope.errors = data.errors;
+        } else {
+          alert("Something went wrong.");
+        }
+      });
+  };
 
   $scope.operations = ['+', '-', '*', '/'];
 
-  }]);
+}]);
